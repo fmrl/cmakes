3932aed54f07746d288954ac2431085b4c195269
michael lowell roberts <mlr@fmrl.org>
2012-08-13 20:05:11 -0700

    changes needed for blanchett's first test to run.

--- nyan.cmake
+++ nyan.cmake
@@ -10,16 +10,17 @@
 # ,$
 
 include(cmakes/config.cmake)
+include(cmakes/yaml-cpp.cmake)
 
-find_library(YAMLCPP_LIBRARY libyaml-cpp.a
-	HINTS "${YAMLCPP_PREFIX}"
-	PATH_SUFFIXES lib
-	)
+find_library(
+	NYAN_LIBRARY libnyan.a
+	HINTS "${NYAN_PREFIX}"
+	PATH_SUFFIXES lib)
 
-if(YAMLCPP_LIBRARY)
-	if (NOT DEFINED YAMLCPP_PREFIX)
-		get_filename_component(YAMLCPP_PREFIX 
-			"${YAMLCPP_LIBRARY}" PATH)
+if(NYAN_LIBRARY)
+	if (NOT DEFINED NYAN_PREFIX)
+		get_filename_component(NYAN_PREFIX 
+			"${NYAN_LIBRARY}" PATH)
 		# on platforms where multi-configuration builds are supported,
 		# the libraries are placed in the appropriate sub-directory of
 		# the 'lib" directory.
@@ -27,23 +28,23 @@ if(YAMLCPP_LIBRARY)
 		# libyaml-cpp though.
 		# [todo] i want to cannonize this test into a function.
 		if("${CMAKE_CONFIGURATION_TYPES}")
-			get_filename_component(YAMLCPP_PREFIX 
-				"${YAMLCPP_PREFIX}/../.." ABSOLUTE)
+			get_filename_component(NYAN_PREFIX 
+				"${NYAN_PREFIX}/../.." ABSOLUTE)
 		else()
-			get_filename_component(YAMLCPP_PREFIX 
-				"${YAMLCPP_PREFIX}/.." ABSOLUTE)
+			get_filename_component(NYAN_PREFIX 
+				"${NYAN_PREFIX}/.." ABSOLUTE)
 		endif()
 	endif()
 
-	set(YAMLCPP_LIBRARIES ${YAMLCPP_LIBRARY})
-	set(YAMLCPP_INCLUDE "${YAMLCPP_PREFIX}/include")
-	include_directories(${YAMLCPP_INCLUDE})
+	# [mlr][todo] boost should be included in this.
+	set(NYAN_INCLUDE "${NYAN_PREFIX}/include" ${YAMLCPP_INCLUDE})
+	set(NYAN_LIBRARIES ${NYAN_LIBRARY} ${YAMLCPP_LIBRARIES})
+	include_directories(${NYAN_INCLUDE})
 
-	can_has(yamlcpp)
-	can_has(yaml)
+	can_has(nyan)
 else()
 	message(WARNING 
-		"i could not find libyaml-cpp on your system. please specify the correct path using the YAMLCPP_PREFIX variable.")
+		"i could not find libnyan on your system. please specify the correct path using the NYAN_PREFIX variable.")
 endif()
 
 # $vim:23: vim:set sts=3 sw=3 et:,$
